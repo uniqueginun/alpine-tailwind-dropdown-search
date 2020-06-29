591be06991b02e25703297fdd91ae2e8aee3d48f
Othman Mohammed <uniqueginun@gmail.com>
2020-06-29 17:08:24 +0300

    pick one of suggestions and make it selected

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
 function searchDropdown() {
   return {
-    open: true,
+    open: false,
 
     pickOption({ target }) {
-      console.log(target.innerHTML);
+      let el = this.$refs.select;
+      el.value = "";
+      let options = el.options;
+      Array.from(options).forEach((opt) => {
+        if (target.innerHTML === opt.value) {
+          opt.setAttribute("selected", true);
+        }
+      });
+      this.open = false;
+      document.getElementById("result").innerHTML = "";
+      this.$refs.keyword.value = "";
     },
 
     lookup({ target }) {
       let resultBox = document.getElementById("result");
       resultBox.innerHTML = "";
       let keyword = target.value;
-      if (keyword.length < 3) {
+      if (keyword.length < 1) {
         return;
       }
 
       let dropDown = this.$refs.select;
       Array.from(dropDown.options).forEach((opt) => {
-        if (opt.text.match(keyword)) {
+        if (opt.text.toLowerCase().match(keyword.toLowerCase())) {
           var node = document.createElement("LI");
           node.className =
             "w-48 min-w-full rounded p-3 bg-gray-300 mb-1 cursor-pointer";
